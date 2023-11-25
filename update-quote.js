42b3b0965c58d157392cc68e654f675111ecd317
Kiran <75929997+Kiran1689@users.noreply.github.com>
2023-11-25 22:10:50 +0530

    Update update-quote.js

--- update-quote.js
+++ update-quote.js
@@ -7,12 +7,12 @@ async function updateQuote() {
     const { quote, author } = quotes[randomIndex];
 
     const cardDesign = `
-    <!--STARTS_HERE_QUOTE_CARD-->
-    <p align="center">
-        <img src="https://readme-daily-quotes.vercel.app/api?author=${encodeURIComponent(author)}&quote=${encodeURIComponent(quote)}&theme=dark&bg_color=011627&author_color=ffeb95">
-    </p>
-    <!--ENDS_HERE_QUOTE_CARD-->
-    `;
+<!--STARTS_HERE_QUOTE_CARD-->
+<p align="center">
+    <img src="https://readme-daily-quotes.vercel.app/api?author=${encodeURIComponent(author)}&quote=${encodeURIComponent(quote)}&theme=dark&bg_color=011627&author_color=ffeb95">
+</p>
+<!--ENDS_HERE_QUOTE_CARD-->
+`;
 
     const readmePath = './README.md';
     let readmeContent = fs.readFileSync(readmePath, 'utf-8');
